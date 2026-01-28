c8f40aab896a36f4a8f008d98cb7b087c4f25b69
GiuliSGit <giuliachiarini@outlook.com>
2026-01-28 10:05:05 +0100

    fineLavoroSevocale

---
+++ progettoTest1/TestProject1/CFunitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using progettoTest1.services;
+
+namespace TestProject1
+{
+    public class CFunitTest
+    {
+        private CFservice cFservice;
+        [OneTimeSetUp]
+            public void OneTimeSetup()
+        {
+            cFservice = new CFservice();
+        }
+        [TestCase("a", true)]
+        [TestCase("b", false)]
+        [TestCase("e", true)]
+        [TestCase("i", true)]
+        [TestCase("o", true)]
+        [TestCase("u", true)]
+        [TestCase("A", true)]
+        [TestCase("U", true)]
+        [TestCase("&", false)]
+        [TestCase("2", false)]
+
+        public void tdd(char var1, bool var2)
+        {
+            Assert.That(var2, Is.EqualTo(cFservice.sevocale(var1)));
+        }
+    }
+}
---
+++ progettoTest1/progettoTest1/services/CFservice.cs
@@ -0,0 +1,20 @@
+﻿namespace progettoTest1.services
+{
+    public class CFservice
+    {
+        public bool sevocale(char c)
+        {
+            char minuscolo = char.ToLower(c);
+            char[] vocali = { 'a', 'e', 'i', 'o', 'u' };
+
+            foreach (char v in vocali)
+            {
+                if (v == minuscolo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
